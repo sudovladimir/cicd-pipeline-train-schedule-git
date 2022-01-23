74f14aeff49b16253da0593e8b5cc9fcc93920fb
Cloud Assessment User <cloud_user@ip-10-0-1-13.ec2.internal>
2022-01-23 17:06:44 -0500

    error commit

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var trainsRouter = require('./routes/trains');
+var logger = require('morvar trainsRouter = require('./routes/trains');
 
 var app = express();
 
